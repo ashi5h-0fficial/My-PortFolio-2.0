9af55600510a6f8f6ed579af7b139a1fe214f2f9
Ashish Soni <aryaashish745@gmail.com>
2025-11-16 11:53:52 +0530

    pass

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "ASHISH SONI",
+  profile: '/profile.png',
+  designation: "Software Test Engineer",
+  description: "My name is ASHISH SONI. I am a enthusiastic Manul tester in my daily life. I am a quick learner with  self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. My core skill is based on Manual Testing and I love to explore, analyse and find bugs in applications. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'ashish.aumca@gmail.com',
+  phone: '+919076997047',
+  address: '2524/1, Nirala Nagar, Sultanpur 228001 | Uttar Pradesh | INDIA ',
+  github: 'https://github.com/ashi5h-0fficial',
+  facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/ashish-soni-391867130/',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
+  devUsername: "",
+  resume: "https://drive.google.com/file/d/1H0D8LomTFBAkx9geP76LnKZeoiMc71A2/view?usp=sharing"
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import ayla from '/public/image/ayla.jpg';
+import crefin from '/public/image/crefin.jpg';
+import realEstate from '/public/image/real-estate.jpg';
+import travel from '/public/image/travel.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        name: 'ShopperStack',
+        tools: ['FireFlink','JIRA','OpenAI '],
+        description: "ShopperStack is an E-commerce wesite, specially developed for Testing purpose. I thoroughly tested various components of the application by doing Functional, integration and System testing.",
+
+        
+        role: 'Internship Trainee (Manual tester)',
+        // sourceCode: 'https://github.com/ashi5h-0fficial/LeafCart.com', 
+        // demo: '',
+        // image: crefin
+    },
+    {
+        id: 2,
+        name: 'Github',
+        description: 'I performed API Testing on Github application. I created, updated, deleted resources on github using Postman tool.',
+        tools: ["Github", "Microsoft Excel", "Postman"],
+        role: 'Internship trainee',
+        // code: '',
+        // demo: '',
+        // image: travel,
+    },
+    {
+        id: 3,
+        name: 'Spotify',
+        description: 'I performed API Testing on Github application. I created, updated, deleted resources (Playlists) on Spotify application using Postman tool. I have done Authentication 2.0 over here.',
+        tools: ["Github", "Microsoft Excel", "Postman"],
+        role: 'Internship trainee',
+        // code: '',
+        // demo: '',
+        // image: travel,
+    },
+
+
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+//     image: crefin,
+// },
